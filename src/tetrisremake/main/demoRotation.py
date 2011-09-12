1d5bb2cbe5eea9d4603ed8bd7def8888f052fbcc
Christian Santoni <christian.santoni@yahoo.it>
2011-09-12 11:02:42 +0200

    rotationDemo. TOFIX
    Signed-off-by: Christian Santoni <christian.santoni@yahoo.it>

--- src/tetrisremake/main/demoRotation.py
+++ src/tetrisremake/main/demoRotation.py
@@ -9,40 +9,53 @@ from sys import exit
 from objects import *
 from tetrisremake.main.objects.tetramine import Tetramine
 
-def place_tetramine(world, pos, tetra):
-    
-    world[pos[0]][pos[1]] = 1
-    
-    #===========================================================================
-    # for (x,y) in tetra:
-    #    world[pos[0] + y][pos[1] + x] = 2
-    #===========================================================================
-
 
 SCREEN_SIZE = (1000, 1000)
+
+
+class GameWorld(object):
+    
+    def __init__(self, screen_surface, x_resolution, y_resolution):
+        self.x_resolution = x_resolution
+        self.y_resolution = y_resolution
+        
+        self.mino_width = int( SCREEN_SIZE[0] / self.x_resolution )
+        self.mino_height = int( SCREEN_SIZE[1] / self.y_resolution )
+        
+        self.current_tetra = None
+        
+        self.screen_surface = screen_surface
+        
+        self.world_matrix = [[0 for i in range(self.x_resolution)] for j in range(self.y_resolution)]
+        
+        self.pos_matrix = []
+        for x in range(0, SCREEN_SIZE[0], self.mino_width ):
+            temp_pos_array = []
+            for y in range(0, SCREEN_SIZE[1], self.mino_height ):
+                temp_pos_array.append((x, y))
+            self.pos_matrix.append(temp_pos_array)
+        
+    def add_tetramino(self, tetra):
+        self.current_tetra = tetra
+
+    def render(self):
+        
+        #TODO: render grid
+        
+        #TODO: Render non moving blocks
+        
+        #TODO: Render current tetramino
+        self.current_tetra.render(self)
+    
+
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 
-x_resolution = 10
-y_resolution = 10
-
-mino_width = int( SCREEN_SIZE[0] / x_resolution )
-mino_height = int( SCREEN_SIZE[1] / y_resolution )
-
-
-
-world_matrix = [[0 for i in range(x_resolution)] for j in range(y_resolution)]
-
-pos_matrix = []
-for x in range(0, SCREEN_SIZE[0], mino_width ):
-    temp_pos_array = []
-    for y in range(0, SCREEN_SIZE[1], mino_height ):
-        temp_pos_array.append((x, y))
-    pos_matrix.append(temp_pos_array)
+tetris = GameWorld(screen, 10, 10)
 
 print "----------------------------------------"    
-for line in pos_matrix:
+for line in tetris.pos_matrix:
     print line
     
 #===============================================================================
@@ -53,17 +66,27 @@ for line in pos_matrix:
 #    3 = filled block, non-moving
 # 
 #===============================================================================
+
 print "----------------------------------------"
-for line in world_matrix:
+for line in tetris.world_matrix:
     print line
 
-tetra = Tetramine([ (1, 0), (-1, 0), (0, -1) ])
+t = [ (1, 0), (-1, 0), (0, -1) ]
+i = [ (0, -1), (0, 1), (0, 2) ]
+l = [ (0, -1), (0, -2), (1, 0) ]
+s = [ (1, 0), (0, 1), (-1, 1) ]
+z = [ (-1, 0), (0, 1), (1, 1) ]
 
-tetra.place(6,4)
+current_tetra = Tetramine(z, 0)
+
+current_tetra.place(4,4)
+
+tetris.add_tetramino(current_tetra)
+
 
 print "----------------------------------------"
 
-for line in world_matrix:
+for line in tetris.world_matrix:
     print line
 
 while True:
@@ -73,6 +96,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
         
+        pressed_keys = pygame.key.get_pressed()
+        
+        if pressed_keys[pygame.K_LEFT]:
+            tetris.current_tetra.orientation = (tetris.current_tetra.orientation - 1) % 4
+        if pressed_keys[pygame.K_RIGHT]:
+            tetris.current_tetra.orientation = (tetris.current_tetra.orientation + 1) % 4
+            
+        screen.fill((0, 0, 0))  
+          
+        tetris.render()
+        
+        pygame.display.update()
             
--- src/tetrisremake/main/objects/tetramine.py
+++ src/tetrisremake/main/objects/tetramine.py
@@ -4,23 +4,64 @@ Created on 11/set/2011
 @author: Christian
 '''
 
+import pygame
+
 class Tetramine(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, scheme):
+    def __init__(self, scheme, orientation):
         '''
         Constructor
         '''
         self.scheme = scheme
+        self.orientation = orientation
         self.x = 0
         self.y = 0
         
-    def  render(self, world, surface):
-        pass
-    
+    def  render(self, world):
+        
+        pygame.draw.rect(world.screen_surface, (255, 255, 255), (world.pos_matrix[self.x][self.y][0], world.pos_matrix[self.x][self.y][1], world.mino_width, world.mino_height), 1)
+        
+        if self.orientation == 0:
+            for (x, y) in self.scheme:
+                pygame.draw.rect(world.screen_surface, 
+                                 (255, 255, 255), 
+                                 (world.pos_matrix[self.x + x][self.y + y][0], 
+                                  world.pos_matrix[self.x + x][self.y + y][1], 
+                                  world.mino_width, 
+                                  world.mino_height), 
+                                  1)
+        elif self.orientation == 1:
+            for (x, y) in self.scheme:
+                pygame.draw.rect(world.screen_surface, 
+                                 (255, 255, 255), 
+                                 (world.pos_matrix[self.x - y][self.y - x][0], 
+                                  world.pos_matrix[self.x - y][self.y + x][1], 
+                                  world.mino_width, 
+                                  world.mino_height), 
+                                  1)
+        elif self.orientation == 2:
+            for (x, y) in self.scheme:
+                pygame.draw.rect(world.screen_surface, 
+                                 (255, 255, 255), 
+                                 (world.pos_matrix[self.x - x][self.y - y][0], 
+                                  world.pos_matrix[self.x - x][self.y - y][1], 
+                                  world.mino_width, 
+                                  world.mino_height), 
+                                  1)
+        elif self.orientation == 3:
+            for (x, y) in self.scheme:
+                pygame.draw.rect(world.screen_surface, 
+                                 (255, 255, 255), 
+                                 (world.pos_matrix[self.x + y][self.y + x][0], 
+                                  world.pos_matrix[self.x + y][self.y - x][1], 
+                                  world.mino_width, 
+                                  world.mino_height), 
+                                  1)
+        
     def place(self, x, y):
         self.x = x
         self.y = y
